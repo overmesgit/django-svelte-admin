f75d717c613735733ea7653c27cb4bfc680da15e
Artem Bezu <artem.bezu@localfolio.co.jp>
2021-08-07 22:53:31 +0900

    Fix generator

--- svelte-typescript-app/generateClient.sh
+++ svelte-typescript-app/generateClient.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 ./../manage.py generateschema --file ../schema.json --format openapi-json
 ./node_modules/.bin/openapi-generator-cli generate -i ../schema.json -g typescript-fetch -o src/client-ts/
-./node_modules/.bin/openapi-generator-cli generate -i ../schema.json -g typescript-fetch -o src/client-ts/ --global-property models="Author:Book:Group:User",api -t out
+./node_modules/.bin/openapi-generator-cli generate -i ../schema.json -g typescript-fetch -o src/client-ts/ --global-property apis,supportingFiles,models="Author:Book:Group:User" -t out --additional-properties=useSingleRequestParameter=false
